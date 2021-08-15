fb7d72e50a580bf48a31c99f0e9d4160375b1b1a
Shiki <ryougi.shiki.kong@outlook.com>
2021-08-15 20:40:52 +0800

    docs: add script description.

--- 4.modules/6-zsh.sh
+++ 4.modules/6-zsh.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#================================================
+# 6-zsh.sh
+# 
+# - Author: Chengtao Yao
+# - Email: chengtao.yao@outlook.com
+# - Last Modified: 2021-08-15 20:34:32 
+#
+# This script installs ZSH and OH-MY-ZSH.
+#================================================
+
+# Source system vars and format output script.
 if [ -z $SYSTEM_VARIABLES_SOURCED ]; then
     source ./3.include/1-system-variables.sh
 fi
@@ -8,18 +19,22 @@ if [ -z $FORMAT_SOURCED ];then
     source ./3.include/2-format-output.sh
 fi
 
+# Install ZSH
 install_zsh() {
     status $(echo $SHELL)
     sudo apt update
     sudo apt install -y zsh
 }
 
+
+# Install OH-MY-ZSH
 install_oh_my_zsh(){
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-syntax-highlighting
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh}/plugins/zsh-autosuggestions
 }
+
 
 case $os in
     Linux*)
--- 4.modules/7-dotfile-setup.sh
+++ 4.modules/7-dotfile-setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#================================================
+# 7-dotfile-setup.sh
+# 
+# - Author: Chengtao Yao
+# - Email: chengtao.yao@outlook.com
+# - Last Modified: 2021-08-15 20:39:57
+#
+# This script installs themes and icons.
+#================================================
+
+# Source system vars and format output script.
 if [ -z $SYSTEM_VARIABLES_SOURCED ]; then
     source ./3.include/1-system-variables.sh
 fi
@@ -8,6 +19,8 @@ if [ -z $FORMAT_SOURCED ];then
     source ./3.include/2-format-output.sh
 fi
 
+
+# Neovim Configuration.
 set_neovim() {
     if [[ -d ~/.config/nvim ]]; then
         cp -rf ./5.dotfiles/nvim ~/.config/
