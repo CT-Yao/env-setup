be59ccc37e84ae20fe03987c735f16cf70fb94fa
Shiki <ryougi.shiki.kong@outlook.com>
2021-08-15 19:42:41 +0800

    docs: add script description.

--- 4.modules/1-system-info.sh
+++ 4.modules/1-system-info.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
-# This script displays system info.
 
+#================================================
+# 1-system-info.sh
+#
+# - Author: Chengtao Yao
+# - Email: chengtao.yao@outlook.com
+# - Last Modified: 2021-08-15 19:38:55 
+#
+# This script show system info like OS, kernel.
+#================================================
+
+# Source system vars and format output script.
 if [ -z $SYSTEM_VARIABLES_SOURCED ]; then
-	source ./3.include/1-system-variables.sh
+    source ./3.include/1-system-variables.sh
 fi
 
 if [ -z $FORMAT_SOURCED ];then
-	source ./3.include/2-format-output.sh
+    source ./3.include/2-format-output.sh
 fi
 
-################     Display System Info     ################
+# Show system info.
 system_info() {
-	status "(*) System Info:"
-	echo "    - User: $USER"
-	echo "    - Hostname: $HOSTNAME"
-	if [[ ! -z $DISTRIB_DESRIPTION ]]; then
-		echo "    - OS: $DISTRIB_DESRIPTION - $DISTRIB_CODENAME"
-	else
-		echo "    - OS: $os"
-	fi
-	echo "    - Arcitecture: $os_architecture"
-	echo "    - Kernel: $os_kernel"
+    status "(*) System Info:"
+    echo "  - User: $USER"
+    echo "  - Hostname: $HOSTNAME"
+    if [[ ! -z $DISTRIB_DESRIPTION ]]; then
+    	echo "  - OS: $DISTRIB_DESRIPTION - $DISTRIB_CODENAME"
+    else
+    	echo "  - OS: $os"
+    fi
+    echo "  - Arcitecture: $os_architecture"
+    echo "  - Kernel: $os_kernel"
 }
 
 system_info
